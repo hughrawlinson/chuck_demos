210f94273a454ec9aeefe6c18fe3efbe07a8aba0
Hugh Rawlinson <hugh@spotify.com>
2016-06-09 19:27:35 +0200

    Set up Travis

--- upload.py
+++ upload.py
@@ -1,2 +1,4 @@
 if __name__ == "__main__":
-  print("Stubbed Soundcloud Upload Script reporting for duty");
+    # Use env TRAVIS_PULL_REQUEST to determine behaviour.
+    # Private with link posted to gh maybe?
+    print("Stubbed Soundcloud Upload Script reporting for duty")
